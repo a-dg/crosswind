42861b27cab430c6b18ab48a2b23fb6df4677be9
Alan Greene <alan@basicrecipe.com>
2023-05-11 15:18:01 -0700

    Add breakpoint detector

--- incl/footer.php
+++ incl/footer.php
@@ -1,2 +1,23 @@
+<div class="breakpoint-detector">
+	<!-- <div class="progress"></div> -->
+	<?
+	$breakpoints = [
+		'zo',
+		'tn',
+		'sm',
+		'md',
+		'av',
+		'ch',
+		'lg',
+		'xl',
+		'yl',
+	];
+	?>
+	<? foreach ($breakpoints as $breakpoint): ?>
+		<span class="<?= $breakpoint ?>"><?= $breakpoint ?></span>
+	<? endforeach; ?>
+</div>
+
+
 </body>
 </html>
