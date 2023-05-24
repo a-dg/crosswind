99c69d5897749eb1f64e1049341147579c045964
Alan Greene <alan@basicrecipe.com>
2023-05-23 19:57:01 -0700

    Organize files for configurability

--- incl/header.php
+++ incl/header.php
@@ -4,7 +4,7 @@
 <title>Grid demo</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<link rel="stylesheet" type="text/css" media="all" href="/css/crosswind.css">
+<link rel="stylesheet" type="text/css" media="all" href="/css/index.css">
 </head>
 
 <body>
